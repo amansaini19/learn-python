6b3dab5bd262b8a8ec6115d6c29c1afcf9140edd
Amandeep Saini <amans19@hotmail.com>
2020-11-29 08:34:25 -0800

    if-then and functions

--- learn.py
+++ learn.py
@@ -8,3 +8,18 @@ print("Product : " , i*j)
 print("Modulo : " , i%j)
 print("Floor Division : " , i//j)
 print("Float Division : " , i/j)
+
+
+# if-then
+def weirdOrNot(n):
+    if n%2 == 1  :
+        print("Weird")
+    elif n >=2 and n <=5 :
+        print("Not Weird")
+    elif n >=6 and n <=20 :
+        print("Weird")
+    elif n > 20 :
+        print("Not Weird")
+
+weirdOrNot(2)
+weirdOrNot(3)
