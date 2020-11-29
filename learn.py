2452268eaa2af431027f12fcf4465bb113ef6c39
Amandeep Saini <amans19@hotmail.com>
2020-11-29 08:13:00 -0800

    Print with operators

--- learn.py
+++ learn.py
@@ -1,1 +1,10 @@
 print("Hello World!")
+
+i = 5;
+j = 3;
+print("Sum : ", i+j)
+print("Diff : " , i-j)
+print("Product : " , i*j)
+print("Modulo : " , i%j)
+print("Floor Division : " , i//j)
+print("Float Division : " , i/j)
